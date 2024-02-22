7a35eb751d85f63215492163360ca071818731c6
Muhammad imran khan <muhammadimran752310@gmail.com>
2024-02-22 17:56:38 +0500

    class

---
+++ 01_start/chai.py
@@ -0,0 +1,4 @@
+from collections import ChainMap
+from class1 import Chai
+
+ChainMap("Imran Swati")
---
+++ 01_start/class1.py
@@ -0,0 +1,12 @@
+# print("pakistan")
+# print("pakistan")
+# print("pakistan")
+
+accontId= 1234
+accountName:any = "imran"
+print(accontId)
+print(type(accontId))
+print(accountName)
+print(type(accountName))
+print(id(accountName))
+
